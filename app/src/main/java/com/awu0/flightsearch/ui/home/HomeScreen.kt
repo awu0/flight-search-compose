040325d858ec165e2c59cf4ce06c13ffcb2642c2
Aaron Wu <82164146+awu0@users.noreply.github.com>
2024-12-06 12:09:13 -0500

    added nonfunctional search bar

--- app/src/main/java/com/awu0/flightsearch/ui/home/HomeScreen.kt
+++ app/src/main/java/com/awu0/flightsearch/ui/home/HomeScreen.kt
@@ -1,12 +1,27 @@
 package com.awu0.flightsearch.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.awu0.flightsearch.model.Airport
 import com.awu0.flightsearch.ui.AppViewModelProvider
@@ -18,7 +33,28 @@ fun HomeScreen(
 ) {
     val homeUiState by viewModel.homeUiState.collectAsState()
 
-    AirportList(homeUiState.flightList)
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxWidth().padding(16.dp)
+    ) {
+        OutlinedTextField(
+            value = viewModel.searchQuery,
+            onValueChange = { viewModel.updateSearchQuery(it) },
+            leadingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = null
+                )
+            },
+            placeholder = {
+                Text("Search")
+            },
+            shape = RoundedCornerShape(64.dp),
+            modifier = Modifier.fillMaxWidth().padding(bottom = 8.dp)
+        )
+
+        AirportList(homeUiState.flightList)
+    }
 }
 
 
@@ -29,7 +65,10 @@ fun AirportList(
 ) {
     LazyColumn {
         items(items = airportList, key = {it.id}) {item ->
-            Text(item.name)
+            Text(
+                text=item.name,
+                fontSize = 24.sp
+            )
         }
     }
 }
--- app/src/main/java/com/awu0/flightsearch/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/awu0/flightsearch/ui/home/HomeViewModel.kt
@@ -1,9 +1,13 @@
 package com.awu0.flightsearch.ui.home
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.awu0.flightsearch.data.AirportRepository
 import com.awu0.flightsearch.model.Airport
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
@@ -14,6 +18,9 @@ data class HomeUiState(val flightList: List<Airport> = listOf())
 
 class HomeViewModel(airportRepository: AirportRepository): ViewModel() {
 
+    var searchQuery by mutableStateOf("")
+        private set
+
     val homeUiState: StateFlow<HomeUiState> =
         airportRepository.getAllAirports().map {HomeUiState(it)}
             .stateIn(
@@ -21,6 +28,10 @@ class HomeViewModel(airportRepository: AirportRepository): ViewModel() {
                 started = SharingStarted.WhileSubscribed(TIMEOUT_MILLIS),
                 initialValue = HomeUiState()
             )
+
+    fun updateSearchQuery(query: String) {
+        searchQuery = query
+    }
 
     companion object {
         private const val TIMEOUT_MILLIS = 5_000L
